a4ef27401f018f814f5c6759f0d0ebcc09f3f2aa
Pierluigi <96800487+Pier1501@users.noreply.github.com>
2024-12-23 16:32:41 +0000

    Fixing DatabaseData class

--- db_data.py
+++ db_data.py
@@ -3,14 +3,8 @@ from dotenv import dotenv_values
 NEO_URL = "bolt://"
 
 class DatabaseData:
-    def __init__(self):
+    def __init__(self, user_ref = "DATABASE_USER", link_ref = "DATABASE_LINK", password_ref = "DATABASE_PASSWORD"):
         self.db_values = dotenv_values(".env")
-
-    def get_password(self, password_ref = "DATABASE_PASSWORD"):
-        return self.db_values[password_ref]
-
-    def get_connection(self, link_ref = "DATABASE_LINK"):
-        return NEO_URL + self.db_values[link_ref]
-    
-    def get_user(self, name_ref = "DATABASE_USER"):
-        return self.db_values[name_ref]
+        self.password = self.db_values[password_ref]
+        self.user = self.db_values[user_ref]
+        self.get_connection = NEO_URL + self.db_values[link_ref]
